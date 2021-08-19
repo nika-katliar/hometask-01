fa6c929d12bf71be88b6f94a3c0de8694c38d6ea
Nika Katliar <katliar.veranika@gmail.com>
2021-08-19 22:22:11 +0300

    js

---
+++ hometask-3/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  var currentFloor = 2;// переменная, где хранится текущий этаж
+  var floorPath =$(".home-image path");// каждый отдельный этаж в svg
+  var counterUp = $(".counter-up"); /*кнопка увеличения этажа*/ 
+  var counterDown = $(".counter-down"); /*кнопка увеличения этажа*/
+  
+  // функция при наведении мышью на этаж
+  floorPath.on("mouseover",function(){
+    floorPath.removeClass("current-floor"); // удаляем активный класс этажей
+    currentFloor = $(this).attr("data-floor");// получаем текущего этажа
+    $(".counter").text(currentFloor);// записываем значение этажа в счетчик справа
+    });
+ 
+counterUp.on("click", function(){// отслеживаем клик по кнопке вверх
+ if (currentFloor < 18) {// проверяем значение этажа не должно бытььь больше 18
+     currentFloor++; //прибавляем один этаж
+  usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, 
+  useGrouping: false}); // форматируем переменную с этажом, что бы было 01 а не 1
+  $(".counter").text(usCurrentFloor);// записываем значение этажа в счетчик справа
+   floorPath.removeClass("current-floor");// удаляем активный класс этажей
+   $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");// подсвечиваем текущий этаж
+ }
+});
+
+counterDown.on("click", function () {
+  if (currentFloor > 2) {
+    currentFloor--;
+    usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, 
+  useGrouping: false});
+    $(".counter").text(usCurrentFloor);
+    floorPath.removeClass("current-floor");
+    $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+  }
+})
+});
